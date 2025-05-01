5f3c406f0b28835847c3bc14d56c7ef6ecbdaf1c
masinoex <masinoex@dukes.jmu.edu>
2025-05-01 15:59:38 -0400

    donnnneeee

---
+++ p4-sockets/mirror-exec.c
@@ -0,0 +1,159 @@
+/*********************************************************************
+    PA-04:  Sockets
+
+    FILE:   mirror-exec.c   SKELETON
+
+    Written By:
+                Abigail Ensogna and Elvis Masinovic
+
+    Submitted on:   <PUT DATE  HERE >
+**********************************************************************/
+
+#include "myNetLib.h"
+
+void reaper (int sig);      // Handle SIGCHLD
+void killHandler (int sig); // Handle SIGTERM
+
+//------------------------------------------------------------
+int
+main (int argc, char *argv[])
+{
+  int sd_listen,              // Receiving Socket descriptor to Server
+      sd_audit;               // Socket descriptor with Auditor
+  int queLen = 10;            // Max #of pending connection requests
+  struct sockaddr_in cl_addr; // the address of a client
+
+  char *developerName = "Abigail Ensogna and Elvis Masinovic";
+
+  printf ("\n****  Mirror Server **** by %s\n\n", developerName);
+
+  char *auditorIP = AUDITOR_IP; // Default Auditor Server
+
+  // Get the optional Auditon IP address from argv, otherwise use default
+  switch (argc)
+    {
+    case 2:
+      auditorIP = argv[1];
+
+    case 1:
+
+    default:
+      break;
+    }
+
+  printf ("Working with these arguments:\n");
+  printf ("\tAuditor Server IP is '%s'\n", auditorIP);
+
+  // Create a TCP socket bound to this local port MIRROR_TCP_PORT
+  sd_listen = socketTCP (MIRROR_TCP_PORT, NULL, 0);
+
+  // Now, start listening on this TCP port
+  Listen (sd_listen, queLen);
+
+  printf ("Mirror Server Started. Listening at socket %hu\n", sd_listen);
+
+  
+  // This block to be implemented in Phase Two
+
+  // Create a UDP socket with ephemeral port, but 'connected' to the Auditor
+  // server
+  sd_audit = socketUDP (0, auditorIP, AUDITOR_UDP_PORT);
+  
+
+  /* Let reaper clean up after completed child processes */
+  sigactionWrapper (SIGCHLD, &reaper);
+
+  /* Let killHandler() handle CTRL-C or a KILL command from terminal*/
+  sigactionWrapper (SIGKILL, &killHandler);
+
+  // For ever, wait till clients connect to me
+  // Will only terminate when I receive a 'SIGTERM'
+  while (1)
+    {
+      unsigned int clAddrLen = sizeof (cl_addr);
+      int sd_clnt;
+
+      // Wait for a client to connect & record the 'accepted' socket in sd_clnt
+      sd_clnt = Accept (sd_listen, (struct sockaddr *)&cl_addr,
+                        (socklen_t *)&clAddrLen);
+
+      struct sockaddr_in clientSocket;
+      unsigned int cliSockLen = sizeof (clientSocket);
+      char ipStr[20];
+
+      if (getpeername (sd_clnt, (struct sockaddr *)&clientSocket,
+                       (socklen_t *)&cliSockLen)
+          != 0)
+        {
+          err_quit ("getpeername error");
+        }
+
+      if (inet_ntop (AF_INET, (void *)&clientSocket.sin_addr, ipStr,
+                     sizeof (clientSocket))
+          == NULL)
+        {
+          err_quit ("inet ntop error");
+        }
+
+      // Display IP : Port of the client
+      printf ("\nServer just accepted connection from %s : %hu\n", ipStr, ntohs (clientSocket.sin_port));
+
+      // Delegate a sub-server child process to handle this client
+      // Start a subMirror server using one of the 'exec' family of system
+      pid_t pid = Fork ();
+      if (pid < 0) {
+        err_quit("fork error");
+      }
+      else if (pid == 0)
+        {
+          // Child
+          char sd_clnt_str[20];
+          char sd_audit_str[20];
+
+          snprintf(sd_clnt_str, 20, "%d", sd_clnt); 
+          snprintf(sd_audit_str, 20, "%d", sd_audit); 
+
+          execl ("./subMirror", "subMirror", sd_clnt_str, sd_audit_str, NULL);
+        }
+      // calls Pass the 'sd_clnt'  and  'sd_audit' to that subServer
+
+      // As for the parent server, make sure you close sockets you don't need
+      else
+        {
+          Close (sd_clnt);
+        }
+    }
+
+  return 0;
+}
+
+/*------------------------------------------------------------------------
+ * reaper - clean up zombie children
+ *------------------------------------------------------------------------
+ */
+
+void
+reaper (int sig)
+{
+  pid_t pid;
+  int status;
+
+  // Don't know how many signals, so loop till there are still
+  // more signals from child processes
+
+  while ((pid = waitpid (-1, &status, WNOHANG)) > 0)
+    fprintf (stderr, "\nA Child server process %d has terminated\n", pid);
+
+  return;
+}
+
+/*------------------------------------------------------------------------
+ * killHandler - clean up after receiving a KILL signal
+ *------------------------------------------------------------------------
+ */
+void
+killHandler (int sig)
+{
+  fprintf (stderr, "\nThe Mirror Server is now closing\n");
+  exit (0);
+}
---
+++ p4-sockets/subMirror.c
@@ -0,0 +1,92 @@
+/*********************************************************************
+    PA-04:  Sockets
+
+    FILE:   subMirror.c   SKELETON
+
+    Written By:
+                1- Abigail Ensogna and Elvis Masinovic
+
+    Submitted on:   <PUT DATE  HERE >
+**********************************************************************/
+
+#include "myNetLib.h"
+#define MAXSTRLEN 256
+#define CHUNK_SZ 1000
+
+/*------------------------------------------------------------------------
+ * This is a child server attending to an incoming client on 'sd'
+ * Audit activities to the Auditor via 'sd_audit'
+ *------------------------------------------------------------------------
+ */
+
+int
+main (int argc, char *argv[])
+{
+  int sd, sd_audit;
+  char ipStr[MAXSTRLEN];
+  char chunk[CHUNK_SZ];
+
+  int alen;
+  struct sockaddr_in clntaddr;
+
+  char *developerName = "Abigail Ensogna and Elvis Masinovic";
+
+  printf ("\n****  sub-Mirror Server **** by %s\n\n", developerName);
+
+  // Get the required  socket descriptors of the Client
+  // and of the Auditor from the command line arguments
+  sd = atoi (argv[1]);       // client connected TCP socket
+  sd_audit = atoi (argv[2]); // Auditor UDP socket
+
+  // what is the difference between what the top is supposed to
+  // do vs what this block below is supposed to do
+  // This block to be implemented in Phase Two
+
+  audit_t activity;          // activity auditing
+  //sd_audit = atoi (argv[2]); // Auditor UDP socket
+
+  // find out my IP:Port of the client from
+  // the provided socket descriptors
+  alen = sizeof (clntaddr);
+
+  if (getsockname (sd, (SA *)&clntaddr, &alen) < 0)
+    err_quit ("getsockname error");
+
+  if (!inet_ntop (AF_INET, &clntaddr.sin_addr, ipStr, MAXSTRLEN))
+    err_quit ("inet_ntop error");
+
+  while (1) // Loop until client closes socket
+    {
+      // Get a chunk of data from the client. Wisely choose which
+      // variant of the read() wrappers to use here
+      ssize_t numRead = Read (sd, chunk, CHUNK_SZ);
+      if (numRead == 0) {
+        break;
+      }
+
+      // This block to be implemented in Phase Two
+
+      // Report this receive activity to the Auditor
+      activity.op = 2;
+      activity.nBytes = numRead;
+      activity.ip = clntaddr.sin_addr.s_addr;
+      // sendto(....)
+      writen(sd_audit, &activity, sizeof(activity)); 
+
+      // send all bytes received above back to the client
+      writen(sd, chunk, numRead);
+
+      // This block to be implemented in Phase Two
+
+      // Report this send activity to the Auditor
+      activity.op = 1;
+      activity.nBytes = numRead;
+      activity.ip = clntaddr.sin_addr.s_addr;
+      //sendto(....);
+      writen(sd_audit, &activity, sizeof(activity)); 
+
+      memset (chunk, 0, CHUNK_SZ);
+    }
+
+  Close (sd);
+}
